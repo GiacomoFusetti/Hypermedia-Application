a5abca53f5356f69e6b9263ad63b8986ca7bf90c
GiovanniGianola <10451768@polimi.it>
2019-05-02 23:02:43 +0200

    authors

--- other/controllers/Session.js
+++ other/controllers/Session.js
@@ -3,7 +3,7 @@
 var utils = require("../utils/writer.js");
 
 module.exports.sessionGET = function sessionGET(req, res, next) {
-    console.log("sessionGET");
+    //console.log("sessionGET");
     var response = []
     
     if(req.session.loggedin) {
