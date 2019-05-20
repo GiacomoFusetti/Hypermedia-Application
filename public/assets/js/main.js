00be0a6c606ab92bc26bd8fbc8c0eb3004b0fe8d
GiovanniGianola <10451768@polimi.it>
2019-05-20 18:12:35 +0200

    style fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -113,7 +113,7 @@ jQuery(document).ready(function($) {
     });
   
   // Preloader
-  $(window).on('load', function () {
+  $(window).ready(function () {
     if ($('#preloader').length) {
       $('#preloader').delay(100).fadeOut('slow', function () {
         $(this).remove();
@@ -191,57 +191,7 @@ jQuery(document).ready(function($) {
 		} else {
 			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
 		}
-	});
-    
-
-
-	/*--/ Property owl owl /--*/
-	$('#book-carousel').owlCarousel({
-		loop: true,
-		margin: 0,  
-		nav: true,
-		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
-
-	/*--/ News owl /--*/
-	$('#new-carousel').owlCarousel({
-		loop: true,
-		margin: 30,
-		responsive: {
-			0: {  
-				items: 1,
-			},
-			769: {
-				items: 2,
-			},
-			992: {
-				items: 3,
-			}
-		}
-	});
-
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-carousel').owlCarousel({
-		margin: 0,
-		autoplay: true,
-		nav: true,
-		animateOut: 'fadeOut',
-		animateIn: 'fadeInUp',
-		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
-    
+	});   
 
 });
 
