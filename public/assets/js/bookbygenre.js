b3a386948b81b78e9f730e61ac504169e08131c6
GiacomoFusetti <32344787+GiacomoFusetti@users.noreply.github.com>
2019-06-10 16:22:48 +0200

    genre

--- public/assets/js/bookbygenre.js
+++ public/assets/js/bookbygenre.js
@@ -2,6 +2,7 @@ console.log("Loading book_by_genre page");
 new WOW().init();
 
 let genreJson;
+let list = [];
 
 $(document).ready(function(){
 	getGenre();
@@ -16,6 +17,7 @@ function getGenre(){
 	}).then(function(json) {
 		genreJson = json;
 		if(!jQuery.isEmptyObject(genreJson)){
+            genreJson.sort((a, b) => a.name.localeCompare(b.name));
 			generatesGenreHTML();
 		}else{
 			// TODO
@@ -26,19 +28,44 @@ function getGenre(){
 // -------------- GENERATES HTML ---------------
 
 function generatesGenreHTML(){
+    generatesLetterList();
     
+    var genreHTML = `
+                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                            <li class="list-unstyled"><div class="title-box-d"><h3 class="title-d">` + list[0] + `</h3></div><ul class="list-unstyled">`;
+    
+    var z = 0;
     for(x = 0; x < genreJson.length; x++){
-        var genre = genreJson[x];    
-        $('#bodyDiv').append( 
-                `<div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
-					<div class="category-books books grid-view">
-						<div class="title-box-d section-t1">
-							<h3 class="title-d">
-								<a class="pointer-a link_style" href='books.html?genre=${genre.id_genre}'>${genre.name}</a>
-							</h3>
-						</div>
-					</div>
-                </div>`
-        );
-    }	
+        var genre = genreJson[x];
+        if(!genre.name.toUpperCase().startsWith(list[z])){
+            z++;
+            //stampare la nuova lettera di list
+            genreHTML += `
+                            </ul></li></div>
+                            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                                <li class="list-unstyled"><div class="title-box-d"><h3 class="title-d">` + list[z] + `</h3></div><ul class="list-unstyled">
+                         `;
+        }
+        //stampo genre nella lista non ordinata
+        genreHTML += `
+                        <li  class="item-list-a"><i class="fa fa-angle-right"></i>
+                            
+                                <a class="pointer-a link_style list-font" href='books.html?genre=${genre.id_genre}'>${genre.name}</a>
+                            
+                        </li>
+                     `;
+    }
+    genreHTML += `</div>`;
+    $('#bodyDiv').html(genreHTML);
+}
+
+function generatesLetterList(){
+    
+    for(y = 0; y <genreJson.length; y++){
+        var letter = genreJson[y].name.substring(0,1).toUpperCase();
+        if(list.indexOf(letter) === -1){
+            list.push(letter);
+        }
+    }
+    console.log(list);
 }
